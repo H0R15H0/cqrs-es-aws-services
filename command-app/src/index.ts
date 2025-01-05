9949e2741eeed0ed356b68f807851435f30bba3a
H0R15H0 <shohori13@gmail.com>
2025-01-05 17:10:01 +0900

    Add endpoint to create an item with RegisterItemService

--- command-app/src/index.ts
+++ command-app/src/index.ts
@@ -1,10 +1,22 @@
 import { serve } from "@hono/node-server";
+import { RegisterItemService } from "application/item";
 import { Hono } from "hono";
+import { createRepositories } from "infrastructure/repositories/common";
 
 const app = new Hono();
 
 app.get("/", (c) => {
 	return c.text("Hello Hono!");
+});
+
+app.post("/createItem", async (c) => {
+	const body = await c.req.json();
+	const command = { name: body.name, price: body.price };
+	const result = await RegisterItemService(command, createRepositories());
+	if (result.isErr()) {
+		return c.json({ error: result.error });
+	}
+	return c.json({ success: true });
 });
 
 const port = 3000;
